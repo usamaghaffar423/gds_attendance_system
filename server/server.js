a75ff6bed4e9f787ab854cb47964ac3bdedb172a
usamaghaffar423 <ossamaghaffar888@gmail.com>
2024-08-28 04:12:27 +0500

    updated package.json file and added vercel.json file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3002, "0.0.0.0", () => {
+  console.log(`Server running on port 3002`);
 });
